e442eb018f81343f7affeb19e177b97f0a1cd8a1
Charlyn Buchanan <charlyn.buchanan@gmail.com>
2015-02-02 18:57:17 -0500

    no message

--- Assesments/04/Lesson04/ArrayViewController.swift
+++ Assesments/04/Lesson04/ArrayViewController.swift
@@ -8,16 +8,62 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    var groceryList = ["milk", "eggs", "cheese", "flour", "apples"]
+    
+    @IBOutlet weak var arrayEntryTextField: UITextField!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        arrayEntryTextField.delegate = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceryList.count
+    }
+    
+    @IBOutlet weak var groceryTableView: UITableView!
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+         var text = arrayEntryTextField.text
+        groceryList.append(arrayEntryTextField.text)
+        groceryTableView.reloadData()
+        let tableViewHeight = groceryTableView.bounds.size.height
+        let contentHeight = groceryTableView.contentSize.height
+        let targetOffset = CGPoint(x: 0, y: contentHeight - tableViewHeight)
+        groceryTableView.setContentOffset(targetOffset, animated: true)
+        return true
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier") as? UITableViewCell ?? UITableViewCell(style: .Default, reuseIdentifier: "CellIdentifier")
+        cell.textLabel?.text = groceryList[indexPath.row]
+        return cell
+    }
+    
+        
+
+        
+        
+        
+//Accept textfield.text when did press returnKey
+//        optional var enablesReturnKeyAutomatically: Bool { get set }
+        
+        
+//Create func to change array (append?) by recieving textfield.text when return key is pressed
+
+        
         
         /*
         TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
+        
         TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
         
         */
+    
     }
-}
-
--- Assesments/04/Lesson04/MapViewController.swift
+++ Assesments/04/Lesson04/MapViewController.swift
@@ -8,15 +8,83 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
+
+    
+    
+    @IBOutlet weak var keyTextField: UITextField!
+    
+    @IBOutlet weak var valueTextField: UITextField!
+    
+    
+    @IBOutlet weak var dictionaryTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /*
-        TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
-        TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
-        TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
-        */
+        keyTextField.delegate = self
+        valueTextField.delegate = self
     }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    var babyAnimalsDictionary = ["Dog":"Puppy", "Cat":"Kitten", "Kangaroo":"Joey"]
+    var babyAnimalsDictionaryKeys: [String] {
+        get {
+            return sorted(babyAnimalsDictionary.keys)
+        }
+    }
+    
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return babyAnimalsDictionaryKeys.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell ?? UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        let key = babyAnimalsDictionaryKeys[indexPath.row]
+        if let value = babyAnimalsDictionary[key] {
+          cell.textLabel?.text = key
+            cell.detailTextLabel?.text = value
+        }
+        return cell
+    }
+    
+//    var arrayOfDictionaries = [[String: String]]()
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let text = valueTextField.text + keyTextField.text
+        //babyAnimalsArray.append(text)
+        if keyTextField == textField {
+            valueTextField.becomeFirstResponder()
+        } else if valueTextField == textField {
+            let key = keyTextField.text
+            let value = valueTextField.text
+            if key.isEmpty || value.isEmpty {
+                return false
+            }
+            
+        }
+        return true
+
+        
+    
+    
+
+    
+    
+    
+
+    
+    /*
+    TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
+    TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
+    TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
+    */
+    
+    
+    
+    
+}
 }
