d4f0fd112a64f7a0212e60927c9d55e73679c5e3
Charlyn Buchanan <charlyn.buchanan@gmail.com>
2015-02-11 19:56:14 -0500

    no message

---
+++ untitled folder/Midterm Calculator/Midterm Calculator/ButtonView.swift
@@ -0,0 +1,13 @@
+//
+//  NumberView.swift
+//  Midterm Calculator
+//
+//  Created by Charlyn Buchanan on 2/11/15.
+//  Copyright (c) 2015 Charlyn Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class NumberView: NSObject {
+   
+}
---
+++ untitled folder/Midterm Calculator/Midterm Calculator/CalculatorButton.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorButton.swift
+//  Midterm Calculator
+//
+//  Created by Charlyn Buchanan on 2/11/15.
+//  Copyright (c) 2015 Charlyn Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
